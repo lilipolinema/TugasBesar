d186aa7e6bd4e058348305a76378c42da0a36d75
megannandafr <56915048+megannandafr@users.noreply.github.com>
2019-12-12 09:07:24 +0700

    Add files via upload

---
+++ Alat_Elektronik/src/Lili/Komputer.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Lili;
+
+/**
+ *
+ * @author USER
+ */
+public class Komputer{
+    Processor proc;
+    
+    public Komputer(Processor proc){
+        this.proc = proc;
+    }
+
+    public void tampil(){
+        System.out.println("Processor");
+        
+    }
+}
---
+++ Alat_Elektronik/src/Lili/Processor.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Lili;
+
+/**
+ *
+ * @author USER
+ */
+public class Processor {
+    public double kecepatan;
+
+    public Processor() {
+        
+        
+    }
+    public Processor(double kecepatan){
+        this.kecepatan = kecepatan;
+
+        System.out.println("Kecepatan : "+kecepatan);
+    }
+}
